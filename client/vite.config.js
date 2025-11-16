ef3dcb1fa38e6549e39d668f04a3b9888f303667
Shiva-2024 <ashutosh979424@gmial.com>
2025-11-16 22:49:25 +0530

    Frontend done

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://realty-ai-price-persona-predictor.onrender.com/',
         changeOrigin: true,
         secure: false,
       },
